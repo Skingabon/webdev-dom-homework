29e16bcf92eedb37446ec9388bcffb47c5ee91fc
skingabon <reclama.katkov@gmail.com>
2023-10-31 10:25:40 +0300

    Делаю 15

--- practice/15/main.js
+++ practice/15/main.js
@@ -1,58 +1,58 @@
 
+import { getTodos, postTodo } from './api.js';
+import { sanitizeHtml } from './sanitizeHtml.js';
+import { renderTodo } from './render.js';
+
+
     const buttonElement = document.getElementById("add-button");
     const listElement = document.getElementById("list");
     const textInputElement = document.getElementById("text-input");
 
-    let tasks = [];
+    export let tasks = [];
 
-    const fetchAndRenderTasks = () => {
-      fetch("https://webdev-hw-api.vercel.app/api/todos", {
-        method: "GET",
-      })
-        .then((response) => {
-          return response.json();
-        })
-        .then((responseData) => {
+    export const fetchAndRenderTasks = () => {
+     getTodos().then((responseData) => {
           tasks = responseData.todos;
-          renderTasks();
+          renderTodo();
           return true;
         });
-    };
+    }
+///////////////////////////////////////////////////////////////
 
-    const renderTasks = () => {
-      const tasksHtml = tasks
-        .map((task) => {
-          return `
-          <li class="task">
-            <p class="task-text">
-              ${task.text}
-              <button data-id="${task.id}" class="button delete-button">Удалить</button>
-            </p>
-          </li>`;
-        })
-        .join("");
+    // const renderTasks = () => {
+    //   const tasksHtml = tasks
+    //     .map((task) => {
+    //       return `
+    //       <li class="task">
+    //         <p class="task-text">
+    //           ${task.text}
+    //           <button data-id="${task.id}" class="button delete-button">Удалить</button>
+    //         </p>
+    //       </li>`;
+    //     })
+    //     .join("");
 
-      listElement.innerHTML = tasksHtml;
-      const deleteButtons = document.querySelectorAll(".delete-button");
+    //   listElement.innerHTML = tasksHtml;
+    //   const deleteButtons = document.querySelectorAll(".delete-button");
 
-      for (const deleteButton of deleteButtons) {
-        deleteButton.addEventListener("click", (event) => {
-          event.stopPropagation();
+    //   for (const deleteButton of deleteButtons) {
+    //     deleteButton.addEventListener("click", (event) => {
+    //       event.stopPropagation();
 
-          const id = deleteButton.dataset.id;
+    //       const id = deleteButton.dataset.id;
 
-          fetch("https://webdev-hw-api.vercel.app/api/todos/" + id, {
-            method: "DELETE",
-          })
-            .then((response) => {
-              return response.json();
-            })
-            .then(() => {
-              fetchAndRenderTasks();
-            });
-        });
-      }
-    };
+    //       fetch("https://webdev-hw-api.vercel.app/api/todos/" + id, {
+    //         method: "DELETE",
+    //       })
+    //         .then((response) => {
+    //           return response.json();
+    //         })
+    //         .then(() => {
+    //           fetchAndRenderTasks();
+    //         });
+    //     });
+    //   }
+    // };
 
     fetchAndRenderTasks();
 
@@ -64,16 +64,18 @@
       buttonElement.disabled = true;
       buttonElement.textContent = "Элемент добавлятся...";
 
-      fetch("https://webdev-hw-api.vercel.app/api/todos", {
-        method: "POST",
-        body: JSON.stringify({
+      // fetch("https://webdev-hw-api.vercel.app/api/todos", {
+      //   method: "POST",
+      //   body: JSON.stringify({
+      //     text: textInputElement.value,
+      //   }),
+      // })
+      //   .then((response) =>{
+      //     return response.json();
+      //   })
+        postTodo({ 
           text: textInputElement.value,
-        }),
-      })
-        .then((response) =>{
-          return response.json();
-        })
-        .then(() => {
+        }).then(() => {
           return fetchAndRenderTasks();
         })
         .then(() => {
@@ -82,10 +84,11 @@
           textInputElement.value = "";
         });
 
-      renderTasks();
+      // renderTasks();
     });
 
-    import {sanitizeHtml} from './sanitizeHtml.js';
+
+
     const tasksHtml = tasks
     .map((task) => {
       return `
